1f9569ac4719898e1eae05bf70068a667af1c28f
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2024-10-28 19:00:06 +0530

    Added styles for works section

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -13,14 +13,15 @@ const Works = () => {
       <section id='works'>
           <h2 className='worksTitle'>My Portfolio </h2>
           <span className='worksDesc'>A showcase of projects that highlight my skills in building efficient, responsive, and user-focused applications. Each project reflects my commitment to quality and innovative problem-solving.</span>
-          <div className='worksImg'>
+          <div className='worksImgs'>
               <img src={Portfolio1} alt='Portfolio1' className='worksImg'/>
               <img src={Portfolio2} alt='Portfolio2' className='worksImg'/>
-              <img src={Portfolio3}alt='Portfolio3' className='worksImg'/>
+              <img src={Portfolio3} alt='Portfolio3' className='worksImg'/>
               <img src={Portfolio4} alt='Portfolio4' className='worksImg'/>
               <img src={Portfolio5} alt='Portfolio5' className='worksImg'/>
               <img src={Portfolio6} alt='Portfolio6' className='worksImg'/>
           </div>
+          <button className='worksBtn'>See More</button>
       </section>
     </div>
   )
