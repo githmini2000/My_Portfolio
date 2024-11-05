dc1ec37cd2ebb8e17803e495f65a330cf73e35f4
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2024-11-05 20:56:45 +0530

    Created footer section

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+ <p>&copy; 2024 Githmini Sandupama.All rights reserved. | Designed and developed with passion.</p>
+    </footer>
+  )
+}
+
+export default Footer;
